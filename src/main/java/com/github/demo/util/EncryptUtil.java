c2a11062aad104b0da239111cd7d27da444d7e10
jihaixiao <xinyi_zhankuang@163.com>
2016-04-24 17:22:07 +0800

    Merge branch 'master' of https://github.com/jihaixiao8/SSMTemplet
    # Conflicts:
    #	src/main/java/com/github/demo/action/BaseController.java

--- src/main/java/com/github/demo/util/EncryptUtil.java
+++ src/main/java/com/github/demo/util/EncryptUtil.java
@@ -1,3 +1,7 @@
+package com.github.demo.util;
+
+import java.security.MessageDigest;
+
 public class EncryptUtil {
     private final static String[] hexDigits = { "0", "1", "2", "3", "4", "5",
             "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
